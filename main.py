534259e05e38ea3ae317a1b68c462db58138ae97
chrislam <lam201786schoolstudent@gmail.com>
2021-12-17 14:34:15 +0800

    DON'T PULL until confirmed it is working!!! test on linux after refactoring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-'''
+"""
 main.py version 1.0.0
 this file will be for combining all the algorithms into one project
-'''
+"""
 
 import pygame
 import math
@@ -9,112 +9,101 @@ import random
 from tkinter import *
 from tkinter import messagebox
 import sys
+
 sys.path.append('./algorithms')
-from dijkstra import *
 from astar import *
 from bfs import *
 from dfs import *
-from ids import *
 from ucs import *
 
+ROW = 50
+COLUMN = 50
 
-ROW=50
-COLUMN=50
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+# start and end nodes
+RED = (223, 50, 100)
+PURPLE = (107, 16, 143)
+# other nodes
+LIGHT_BLUE = (50, 173, 223)
+LIGHT_ORANGE = (255, 155, 119)
+DARK_BLUE = (52, 55, 255)
 
 pygame.init()
-surface=pygame.display.set_mode((650, 650))
-pygame.display.set_caption('Pathfinding Visualizer')
-done = False
+surface = pygame.display.set_mode((650, 650))
+pygame.display.set_caption('Find The Way')
 
-#colors
-BLACK = (0, 0, 0)
-WHITE=(255,255,255)
-#start and end node
-red=(222, 49, 99)
-purple=(106, 15, 142)
-#other nodes
-light_blue=(49, 172, 222)
-light_orange=(255, 154, 118)
-dark_blue=(51, 54, 255 )
-
-
-y0=0
-x1=20
-y1=20
-spacing=5
-x0=0
-
-y=0
-n=20
 
 class Spot:
-    def __init__(self, x, y, col, row, width, distance):
-        self.x=x
-        self.y=y
-        self.col=col
-        self.row=row
-        self.width=width
+    def __init__(self, x, y, column, row, width, weight):
+        self.x = x
+        self.y = y
+        self.column = column
+        self.row = row
+        self.width = width
         self.color = WHITE
-        self.distance=distance
-        self.rect_obj=pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.width))
-        self.font=pygame.font.SysFont('Arial', 15)
-
+        self.weight = weight
+        self.box_obj = pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.width))
+        self.font = pygame.font.SysFont('Arial', 15)
 
     def draw(self):
         pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.width))
 
-    def draw_text(self):
-        text_surface_object = self.font.render(str(self.distance), True, BLACK)
-        text_rect = text_surface_object.get_rect(center=self.rect_obj.center)
-        surface.blit(text_surface_object, text_rect)
+    def draw_weight(self):
+        text_weight = self.font.render(str(self.weight), True, WHITE)
+        text_box_obj = text_weight.get_rect(center=self.box_obj.center)
+        surface.blit(text_weight, text_box_obj)
 
-    def make_start(self):
-        self.color=red
+    def set_source(self):
+        self.color = RED
 
-    def make_end(self):
-        self.color=purple
+    def set_goal(self):
+        self.color = PURPLE
 
-    def make_barrier(self):
-        self.color=BLACK
+    def set_obstacle(self):
+        self.color = BLACK
 
-    def visited_cell(self):
-        self.color=dark_blue
+    def visited_node(self):
+        self.color = DARK_BLUE
 
     def backtrack(self):
-        self.color=light_orange
+        self.color = LIGHT_ORANGE
 
     def edge_color(self):
-        self.color=light_blue
+        self.color = LIGHT_BLUE
 
     def reset(self):
-        self.color=WHITE
+        self.color = WHITE
 
 
-def grid_make(width):
-    grid=[]
-    y0=0
-    n=width
-    for j in range(1 ,ROW+1):
-        gap=width
-        x0=0
+def make_grid(width):
+    grid = []
+    y0 = 0
+    n = width
+    for j in range(1, ROW + 1):
+        gap = width
+        x0 = 0
         grid.append([])
-        for i in range(1, ROW+1):
-            spot=Spot(x0, y0,math.trunc(x0/13),math.trunc(y0/13), width, random.randint(1, 5))
-            x0=gap+i
-            gap=gap+width
-            grid[j-1].append(spot)
-        y0=n+j
-        n=n+width
+        for i in range(1, ROW + 1):
+            spot = Spot(x0, y0, math.trunc(x0 / 13), math.trunc(y0 / 13), width, random.randint(1, 5))
+            x0 = gap + i
+            gap = gap + width
+            grid[j - 1].append(spot)
+        y0 = n + j
+        n = n + width
     return grid
 
-def draw(win, grid):
-    win.fill(BLACK)
+
+def draw(grid_lines, grid):
+    grid_lines.fill(BLACK)
     for row in grid:
         for spot in row:
             spot.draw()
-            spot.draw_text()
+            spot.draw_weight()
 
     pygame.display.flip()
+
 
 '''this will get us the row and column position because
 total width is 650. and number of row and column is 50.
@@ -124,146 +113,150 @@ to change the width and height by some number, then multiply it with 13 and
 give the final value as the window size, because 13 is the default size after 
 adding the spacing etc of the cell here.'''
 
-def get_position(x, y):
-    col=math.trunc(x/13)
-    row=math.trunc(y/13)
-    return col, row
 
-def construct_path(curr_node, from_list, start):
-    r=curr_node
-    for i in range(len(from_list)-1):
-        t=from_list[r]
-        if t==start:
+def get_position(x, y):
+    column = math.trunc(x / 13)
+    row = math.trunc(y / 13)
+    return column, row
+
+
+def construct_path(current_node, from_list, start):
+    current = current_node
+    for i in range(len(from_list) - 1):
+        step = from_list[current]
+        if step == start:
             break
         else:
-            t.backtrack()
-            r=t
+            step.backtrack()
+            current = step
 
-window=Tk()
-window.title('tutorial window')
-t=Text(window, height=30, width=52)
-label=Label(window, text="PathFinding Visualizer Tutorial")
-label.config(font=("Courier", 14))
 
-text="""-----------------------------------------
- ? node Info:\n
-* Red is the start node
-* Purple is the end node
-* Black is a barrier node
-* Dark blue is a visited noide
-* Light blue is node that is currently being explored
-* light Orange is the path node 
--------------------------------------------
- ? how to mark nodes:\n
+window = Tk()
+window.title('Rules and Information')
+text = Text(window, height=30, width=52)
+label = Label(window, text="Find The Way: Rules and Information")
+label.config(font=("Arial", 15))
+
+rules_info_text = """
+-------------------------------------------------
+ ? Node colors:\n
+* Red is the source
+* Purple is the goal
+* Black is a obstacle
+* Dark blue is a explored node
+* Light blue is a visited node
+* Light orange is the path node(s) 
+-------------------------------------------------
+ ? How to mark nodes:\n
 * Right click on the cell to mark the node.
 * Left click on the cell to unmark the node.
-* The order of nodes are:
-  1: Start node
-  2: End node
-  3: Barrier node
--------------------------------------------
+* The order of marking nodes:
+  1: Start node (the source)
+  2: End node (the goal)
+  3: Obstacle node
+-------------------------------------------------
  ? How to visualize algorithms:\n
   To run-
-  BFS       : press 'b'
-  Dijkstra  : press 'u'
-  A* Search : press 'a'
-  DFS       : press 'd'
+  A* (Astar): press key 'a'
+  BFS       : press key 'b'
+  DFS       : press key 'd'
+  UCS       : press key 'u'
 
----------------------------------------------
+-------------------------------------------------
  ? Additional info:\n
-* To clear out the board :Press 'c'
-* To clear out only the visualization:
-  Press the space bar
----------------------------------------------
+* New game  : press key 'c'
+* Replay    : press key 'space bar'
+-------------------------------------------------
 """
 
-t.insert(END, text)
+text.insert(END, rules_info_text)
 
-skip_button=Button(window, text="Skip",width=10, command=window.destroy)
+continue_button = Button(window, text="Continue", width=10, command=window.destroy)
 
 label.grid(columnspan=4, row=0)
-t.grid(columnspan=4, rowspan=7)
-skip_button.grid(column=3, row=1)
+text.grid(columnspan=4, rowspan=7)
+continue_button.grid(column=3, row=1)
 
 window.update()
 mainloop()
 
-def message_box():
-    Tk().wm_withdraw()
-    messagebox.showinfo("error", ('oops!! couldn\'t find a path.\n Maybe it is blocked by barriers. '))
 
-def leave(draw, grid, start, end):
-    if start!=None:
-        start.make_start()
-    if end!=None:
-        end.make_end()
+def warning_message_box():
+    Tk().wm_withdraw()
+    messagebox.showinfo("Error: path not found",
+                        "Failed to find a path from the source to the goal.\n Perhaps it is blocked by obstacles... ")
+
+
+# replay the game again with the same source, goal, random weights, and obstacles.
+def replay(draw, grid, start_node, end_node):
+    if start_node is not None:
+        start_node.set_source()
+    if end_node is not None:
+        end_node.set_goal()
     for row in grid:
-        for col in row:
-            if col.color ==dark_blue or col.color==light_blue or col.color==light_orange:
-                col.color=WHITE
+        for column in row:
+            if column.color == DARK_BLUE or column.color == LIGHT_BLUE or column.color == LIGHT_ORANGE:
+                column.color = WHITE
     draw()
+
 
 def main():
     run = True
 
-    start=None
-    end=None
+    start_node, end_node = None, None
 
-    grid=grid_make(12)
+    grid = make_grid(12)
 
     while run:
         draw(surface, grid)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run=False
-
+                run = False
 
             if pygame.mouse.get_pressed()[0]:
-                x, y=pygame.mouse.get_pos()
-                col, row=get_position(x,y)
-                node=grid[row][col]
-                if not start and node!=end:
-                    start=node
-                    start.make_start()
+                x, y = pygame.mouse.get_pos()
+                column, row = get_position(x, y)
+                selected_node = grid[row][column]
+                if not start_node and selected_node != end_node:
+                    start_node = selected_node
+                    start_node.set_source()
 
-                if not end and node!=start:
-                    end=node
-                    end.make_end()
+                if not end_node and selected_node != start_node:
+                    end_node = selected_node
+                    end_node.set_goal()
 
-                elif node!=start and node!=end:
-                    node.make_barrier()
+                elif selected_node != start_node and selected_node != end_node:
+                    selected_node.set_obstacle()
 
             elif pygame.mouse.get_pressed()[2]:
-                x,y=pygame.mouse.get_pos()
-                col, row=get_position(x, y)
-                node=grid[row][col]
-                node.reset()
-                if node==start:
-                    start=None
-                elif node==end:
-                    end=None
+                x, y = pygame.mouse.get_pos()
+                column, row = get_position(x, y)
+                selected_node = grid[row][column]
+                selected_node.reset()
+                if selected_node == start_node:
+                    start_node = None
+                elif selected_node == end_node:
+                    end_node = None
 
-            if event.type==pygame.KEYDOWN:
-                if event.key==pygame.K_c:
-                    start=None
-                    end=None
-                    grid=grid_make(12)
-                if event.key==pygame.K_SPACE:
-                     leave(lambda: draw(surface, grid),grid, start, end)
+            if event.type == pygame.KEYDOWN:
+                # press key 'c' to start a new game
+                if event.key == pygame.K_c:
+                    start_node, end_node = None, None
+                    grid = make_grid(12)
+                if event.key == pygame.K_SPACE:
+                    replay(lambda: draw(surface, grid), grid, start_node, end_node)
 
-                if event.key==pygame.K_b and start and end:
-                    bfs(lambda: draw(surface, grid), grid, start, end, message_box, construct_path)
-                if event.key==pygame.K_d and start and node:
-                    dfs(lambda: draw(surface, grid), grid, start, end, message_box, construct_path)
-                if event.key==pygame.K_a and start and node:
-                    astar(lambda: draw(surface, grid),grid, start, end, construct_path, message_box)
-                if event.key == pygame.K_u and start and node:
-                    # UCS and Dijkstra are the same
-                    dijkstra(lambda: draw(surface, grid), grid, start, end, message_box, construct_path)
+                if event.key == pygame.K_a and start_node and end_node:
+                    astar(lambda: draw(surface, grid), grid, start_node, end_node, construct_path, warning_message_box)
+                if event.key == pygame.K_b and start_node and end_node:
+                    bfs(lambda: draw(surface, grid), grid, start_node, end_node, warning_message_box, construct_path)
+                if event.key == pygame.K_d and start_node and end_node:
+                    dfs(lambda: draw(surface, grid), grid, start_node, end_node, warning_message_box, construct_path)
+                if event.key == pygame.K_u and start_node and end_node:
+                    ucs(lambda: draw(surface, grid), grid, start_node, end_node, warning_message_box, construct_path)
     pygame.quit()
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
